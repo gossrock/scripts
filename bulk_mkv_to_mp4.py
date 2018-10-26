6ff7bf497f8ce34dc26c1457a60e95f257ce2cb6
gossrock <gossrock@gmail.com>
2018-10-26 13:51:37 +0000

    initial commit of bult_mkv_to_mp4.py script

---
+++ bulk_mkv_to_mp4.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3.6
+
+import glob, shlex
+from utils.command_execution import run
+
+files = glob.glob("*.mkv") # may want to work this into command_execution
+for old_file in files:
+    new_file = old_file[:-4]+".mp4"
+    old_file = shlex.quote(old_file)
+    new_file = shlex.quote(new_file)
+    print(f'converting {old_file} to {new_file}')
+    command = f"ffmpeg -y -i {old_file} -codec copy {new_file}"
+    run(command)
