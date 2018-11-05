6f520e84b1fb1d98fb16bffbbc046f6ba3c49b92
gossrock <gossrock@gmail.com>
2018-11-05 11:25:24 +0000

    changed to use CommandManager to run the commands in parallel. (current set of files takes 1.9s instead of 3.5s to complete).

--- bulk_mkv_to_mp4.py
+++ bulk_mkv_to_mp4.py
@@ -5,13 +5,17 @@ sys.path += [current_dir+'/utils']  # add utils repository to PYTHONPATH
 
 import glob
 import shlex
-from utils.command_execution import run
+from utils.command_execution import Command, CommandManager
 
 files = glob.glob("*.mkv") # may want to work this into command_execution
+
+cm = CommandManager()
+
 for old_file in files:
     new_file = old_file[:-4]+".mp4"
+    print(f'converting {old_file} to {new_file}')
     old_file = shlex.quote(old_file)
     new_file = shlex.quote(new_file)
-    print(f'converting {old_file} to {new_file}')
-    command = f"ffmpeg -y -i {old_file} -codec copy {new_file}"
-    run(command)
+    cm.add(Command(f"ffmpeg -y -i {old_file} -codec copy {new_file}"))
+
+cm.run_all()
